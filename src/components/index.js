64e21b4931d7ad0d8209f4705b50323165290129
Dkhoa_Happy <dangkhoa13978@gmal.com>
2024-11-26 17:24:33 +0700

    Nike Programing

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import ShoeCard from "./ShoeCard";
+import Nav from "./Nav";
+import Button from "./Button";
+import PopularProductCard from "./PopularProductCard";
+import ServiceCard from "./ServiceCard";
+import ReviewCard from "./ReviewCard";
+export { ShoeCard, Nav, Button, PopularProductCard, ServiceCard, ReviewCard };
